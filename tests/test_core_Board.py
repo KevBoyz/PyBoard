f6bd9a82edba6a432f223805b2a94c74fa455e38
KevBoyz <Kevinho_gameplays@hotmail.com>
2023-06-29 15:26:44 -0300

    Project startup
    Passing tests 1 and 2

--- tests/test_core_Board.py
+++ tests/test_core_Board.py
@@ -2,7 +2,7 @@ from PyBoard import *
 
 
 def test_1():
-    board = Board(3, 3, True)
+    board = Board(3, 3, blank_spaces=True)
     board.numerate()
     board[0] = [5, 5, 5]
     board[2][0] = None
